9bf7908e89208f73228030078cb529204f90cbb7
jan-schaefli-github <jan.schaefli@outlook.de>
2023-06-05 15:22:19 +0200

    🗃️

---
+++ Database/database.sql
@@ -0,0 +1,48 @@
+DROP Database IF EXISTS b4u;
+
+CREATE DATABASE b4u;
+
+USE b4u;
+
+-- -----------------------------------------------------
+-- Table clan
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS clan;
+
+CREATE TABLE clan(
+    tag         VARCHAR(100) NOT NULL UNIQUE,
+
+    PRIMARY KEY(tag)
+);
+
+-- -----------------------------------------------------
+-- Table person
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS person;
+
+CREATE TABLE person(
+    tag         VARCHAR(100) NOT NULL UNIQUE,
+    wholeFame   INTEGER NOT NULL DEFAULT 0,
+	clanStatus  BOOLEAN NOT NULL DEFAULT 1,
+    joinDate    DATE DEFAULT (CURRENT_DATE),
+    fk_clan     VARCHAR(100),
+
+    PRIMARY KEY(tag),
+    FOREIGN KEY(fk_clan) REFERENCES clan(tag) ON DELETE SET NULL
+);
+
+-- -----------------------------------------------------
+-- Table weekly_report
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS weekly_report;
+
+CREATE TABLE weekly_report(
+    id	        INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
+    fame	    INTEGER NOT NULL DEFAULT 0,
+    missedDecks INTEGER NOT NULL DEFAULT 0,
+    absenceDays INTEGER NOT NULL DEFAULT 0,
+    fk_person   VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY(id),
+    FOREIGN KEY(fk_person) REFERENCES person(tag) ON DELETE CASCADE
+);
---
+++ Database/testdata.sql
@@ -0,0 +1,3 @@
+INSERT INTO clan (tag) VALUES ('#P9UVQCJV');
+
+INSERT INTO person (tag, wholeFame, clanStatus, joinDate, fk_clan) VALUES ('#2Y9VQVJ9', 0, 1, '2019-01-01', '#P9UVQCJV');
