a5ee48a4915287047a59380efc3f3c386514d9fa
Samuel Muggli <samuel@meatball.ch>
2023-06-15 08:24:57 +0200

    :construction: First functions in new folder structure

--- Backend/endpoints/v1/api/clan/currentriverrace/currentriverrace.go
+++ Backend/endpoints/v1/api/clan/currentriverrace/currentriverrace.go
@@ -1,1 +1,35 @@
 package currentriverrace
+
+import (
+	"b4u/backend/logs"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+)
+
+func getCurrentRiverRaceHandler(c *gin.Context) {
+	urlForApiRequest := "https://api.clashroyale.com/v1/clans/" + encodedClanTag + "/currentriverrace"
+	response, err := makeRequest(urlForApiRequest)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Ein Fehler ist aufgetreten"})
+		logs.logMessage("Request", "Error while making request: "+err.Error())
+		return
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			logs.logMessage("Request", "Error while closing response body: "+err.Error())
+		}
+	}(response.Body)
+
+	var data interface{}
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Ein Fehler ist aufgetreten"})
+		logs.logMessage("Request", "Error while decoding response: "+err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, data)
+}
--- Backend/endpoints/v1/api/clan/members/members.go
+++ Backend/endpoints/v1/api/clan/members/members.go
@@ -1,1 +1,35 @@
 package members
+
+import (
+	"b4u/backend/logs"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+)
+
+func getMembersHandler(c *gin.Context) {
+	urlForApiRequest := "https://api.clashroyale.com/v1/clans/" + encodedClanTag + "/members"
+	response, err := makeRequest(urlForApiRequest)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Ein Fehler ist aufgetreten"})
+		logs.logMessage("Request", "Error while making request: "+err.Error())
+		return
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(response.Body)
+
+	var data interface{}
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Ein Fehler ist aufgetreten"})
+		logs.logMessage("Request", "Error while decoding response: "+err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, data)
+}
