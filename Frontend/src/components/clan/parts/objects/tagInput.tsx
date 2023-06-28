402148811f81154aa1d58fa5914febc438f38a89
Samuel Muggli <samuel@meatball.ch>
2023-06-28 16:19:03 +0200

    :bug: Fixed TagInput.tsx

--- Frontend/src/components/clan/parts/objects/tagInput.tsx
+++ Frontend/src/components/clan/parts/objects/tagInput.tsx
@@ -1,37 +1,46 @@
-import React, { ChangeEvent, KeyboardEvent } from 'react';
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
 
-interface InputComponentProps {
+interface TagInputProps {
     selectedChoice: string;
     errorMessage: string;
-    handleSelectChange: (event: ChangeEvent<HTMLInputElement>) => void;
-    handleKeyDown: (event: KeyboardEvent<HTMLInputElement>) => void;
+    onSearch: (selectedChoice: string) => void;
 }
 
-const InputComponent: React.FC<InputComponentProps> = ({
-                                                           selectedChoice,
-                                                           errorMessage,
-                                                           handleSelectChange,
-                                                           handleKeyDown,
-                                                       }) => (
-    <div className="selection-min">
-        <p className="clan-leaderboard-title">Clan Member Leaderboard</p>
-        <form>
-            <input
-                onChange={handleSelectChange}
-                onKeyDown={handleKeyDown}
-                type="text"
-                name="search"
-                placeholder="Please provide a Clan Tag (#XXXXXXX)"
-                className="form__input"
-                id="clanTag"
-                value={selectedChoice}
-            />
-            <label htmlFor="clanTag" className="form__label">
-                Full Name
-            </label>
-            {errorMessage && <p className="error-message">{errorMessage}</p>}
-        </form>
-    </div>
-);
+const TagInput: React.FC<TagInputProps> = ({ selectedChoice, errorMessage, onSearch }) => {
+    const [inputValue, setInputValue] = useState(selectedChoice);
 
-export default InputComponent;
+    const handleSelectChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            onSearch(inputValue);
+        }
+    };
+
+    return (
+        <div className="selection-min">
+            <p className="clan-leaderboard-title">Clan Member Leaderboard</p>
+            <form>
+                <input
+                    onChange={handleSelectChange}
+                    onKeyDown={handleKeyDown}
+                    type="text"
+                    name="search"
+                    placeholder="Please provide a Clan Tag (#XXXXXXX)"
+                    className="form__input"
+                    id="clanTag"
+                    value={inputValue}
+                />
+                <label htmlFor="clanTag" className="form__label">
+                    Full Name
+                </label>
+                {errorMessage && <p className="error-message">{errorMessage}</p>}
+            </form>
+        </div>
+    );
+};
+
+export default TagInput;
