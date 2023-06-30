c794db4afe95d0b9de47b78faf2c4916a3ddbab9
Samuel Muggli <samuel@meatball.ch>
2023-06-30 09:42:59 +0200

    :children_crossing: Added INFO box and improved responsiveness and dark/light-mode of lineChart

--- Frontend/src/components/clan/parts/clanInfo.tsx
+++ Frontend/src/components/clan/parts/clanInfo.tsx
@@ -1,13 +1,28 @@
-function info() {
-    //TODO: Make a short INFO about tracking and put cool image on the right side for astetics.
+import "../../../assets/css/clan/info.css";
+
+function Info() {
     return (
-        <>
-            <section className="clan-slide" id="part-progress">
-                <h1>Info</h1>
-                <h3>Display Info about the site</h3>
-            </section>
-        </>
+        <section className="clan-slide" id="part-info">
+            <div className="info-container">
+                <div className="info-content">
+                    <h1 id="part-info-title">Boost Your Clan with our Tracking</h1>
+                    <p className="part-info-text">
+                        By signing up for the Tracking service, your clan will be tracked by our system.
+                    </p>
+
+                    <p className="part-info-text">
+                        After a day of tracking, you can check the member Tracking tab for your clan.
+                    </p>
+                    <p className="part-info-text">
+                        Now you have more insights into the activities of your players, a better overview of your clan and the BOOST you need.
+                    </p>
+                </div>
+                <div className="info-image">
+                    <img src="../rocketClashRoyal.avif" alt="Info" />
+                </div>
+            </div>
+        </section>
     );
 }
 
-export default info;
+export default Info;
